ce9e9ee22ce8a530b765317d3c1e879f607b8695
Mootfrost <hello@mootfrost.dev>
2025-09-22 14:17:33 +0300

    Make paginated keyboard generic

--- app/handlers/faq.py
+++ app/handlers/faq.py
@@ -1,62 +1,29 @@
 from aiogram import Router, F
 from aiogram.filters import Command
 from aiogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
-from aiogram.filters.callback_data import CallbackData
+from app.keyboards import get_paginated_keyboard, PaginatedCallbackBase, PaginatedAction
 
 from app.settings import settings
 
 router = Router()
 
-
-class FaqCallback(CallbackData, prefix="faq"):
-    action: str
-    value: int
-
-
-def get_questions_keyboard(questions: list, page: int = 1, per_page: int = 5):
-    keyboard = [
-        [
-            InlineKeyboardButton(
-                text=questions[i],
-                callback_data=FaqCallback(action='ask', value=i).pack()
-            )
-        ]
-        for i in range((page - 1) * per_page,min(page * per_page, len(questions)))
-    ]
-
-    total_pages = (len(questions) + per_page - 1) // per_page
-    if total_pages > 1:
-        nav_buttons = [
-            InlineKeyboardButton(
-                text="⬅️",
-                callback_data=FaqCallback(action='page', value=page - 1 if page > 1 else total_pages).pack()
-            ),
-            InlineKeyboardButton(
-                text=f"{page}/{total_pages}",
-                callback_data=FaqCallback(action='current', value=page).pack()
-            ),
-            InlineKeyboardButton(
-                text="➡️",
-                callback_data=FaqCallback(action='page', value=page + 1 if page < total_pages else 1).pack()
-            ),
-        ]
-        keyboard.append(nav_buttons)
-    return InlineKeyboardMarkup(inline_keyboard=keyboard)
+class FaqCallback(PaginatedCallbackBase, prefix='faq'):
+    pass
 
 
 @router.message(Command('faq'))
 async def start(msg: Message):
     await msg.answer('Выберите вопрос:',
-                     reply_markup=get_questions_keyboard(settings.faq))
+                     reply_markup=get_paginated_keyboard(settings.faq, FaqCallback))
 
 
-@router.callback_query(FaqCallback.filter(F.action == 'page'))
-async def faq_page_callback(callback: CallbackQuery, callback_data=FaqCallback):
+@router.callback_query(FaqCallback.filter(F.action == PaginatedAction.page))
+async def faq_page_callback(callback: CallbackQuery, callback_data=PaginatedCallbackBase):
     await callback.message.edit_reply_markup(
-        reply_markup=get_questions_keyboard(settings.faq, page=callback_data.value))
+        reply_markup=get_paginated_keyboard(settings.faq, FaqCallback, page=callback_data.value))
 
 
-@router.callback_query(FaqCallback.filter(F.action == 'ask'))
+@router.callback_query(FaqCallback.filter(F.action == PaginatedAction.select))
 async def faq_page_callback(callback: CallbackQuery, callback_data=FaqCallback):
     pass
 
